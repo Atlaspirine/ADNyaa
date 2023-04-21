14c0cdc50cdf72b4bcac5b7824e005406f6d7ec0
Grégory Grenot <59602824+Gregory-Grnt@users.noreply.github.com>
2023-04-21 14:50:37 +0200

    ADD: feature blacklist

--- main.py
+++ main.py
@@ -3,21 +3,43 @@ from bs4 import BeautifulSoup
 import datetime
 import time
 import webbrowser
+import os
+
+# Constants
+AGENDA_URL = "https://www.adkami.com/agenda"
+NYAA_SEARCH_URL = "https://nyaa.si/?f=0&c=0_0&q={}"
+USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+BLACKLIST_FILE = "blacklist.txt"
+
+# Functions
+def create_blacklist_file():
+    if not os.path.exists(BLACKLIST_FILE):
+        open(BLACKLIST_FILE, "w").close()
+        print(f"{BLACKLIST_FILE} file created.")
+
+def is_in_blacklist(anime_title):
+    with open(BLACKLIST_FILE, "r") as f:
+        blacklist = f.read().splitlines()
+        return anime_title in blacklist
 
 def search_on_nyaa(title, episode):
     # Merge the title and episode number into a single search query
     search_query = f"{title} vostfr"
-    
-    
+
     # Open the search page on nyaa.si in a new tab
-    webbrowser.open_new_tab(f"https://nyaa.si/?f=0&c=0_0&q={search_query}")
+    webbrowser.open_new_tab(NYAA_SEARCH_URL.format(search_query))
     time.sleep(1)
 
-# Set the URL of the agenda page and make a request
-url = "https://www.adkami.com/agenda"
+# Check if the blacklist file exists and create it if it doesn't
+create_blacklist_file()
+
+# Set the headers for the requests
 headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
-response = requests.get(url, headers=headers)
+    "User-Agent": USER_AGENT
+}
+
+# Make a request to the agenda page
+response = requests.get(AGENDA_URL, headers=headers)
 
 # Parse the HTML content with BeautifulSoup
 soup = BeautifulSoup(response.content, "html.parser")
@@ -45,7 +67,10 @@ for title, episode_info in anime_info:
     # Remove the "Episode" string from the episode info
     episode_info = episode_info.replace("Episode ", "")
     
-    print("- {} (Episode {})".format(title, episode_info))
-    
-    # Search on nyaa.si for the anime
-    search_on_nyaa(title, episode_info)
+    # Check if the anime is in the blacklist
+    if not is_in_blacklist(title):
+        print("O - {} (Episode {})".format(title, episode_info))
+        # Search on nyaa.si for the anime
+        search_on_nyaa(title, episode_info)
+    else:
+        print(f"X - {title} (Episode {episode_info})")
